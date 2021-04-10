9ce9937db37b4e2d2ea7d3827d8c210eeb9d1c10
Genziano <alemaudit@gmail.com>
2021-04-10 15:49:34 +0200

    Basic module structure and indexing implementation

---
+++ setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup, find_packages
+import pathlib
+
+here = pathlib.Path(__file__).parent.resolve()
+long_description = (here / 'README.md').read_text(encoding='utf-8')
+
+
+setup(
+    name='opt-tracker',
+    version='0.0.0dev',
+    description='A lightweight experiment tracker for numerical optimization problems.',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/atraders/opt-tracker',
+    author='@genziano',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+    ],
+    keywords='ml, or, optimization, logging',
+    package_dir={'': 'src'},
+    packages=find_packages(where='src'),
+    python_requires='>=3.6, <4',
+    install_requires=[],
+    extras_require={'dev': ['pylint', 'pytest', 'mypy'],},
+)
---
+++ src/opt_tracker/indexes.py
@@ -0,0 +1,36 @@
+
+from enum import Enum
+from datetime import datetime
+
+
+class Index(Enum):
+    HEADER = 1
+    BEGIN = 1
+    END = 1
+
+
+def int_index_to_str(idx: int) -> str:
+    assert isinstance(idx, int)
+    return f'int{idx}'
+
+
+def str_to_int_index(idx_str: str) -> int:
+    assert isinstance(idx_str, str)
+    assert idx_str.startswith('int')
+    int_str = idx_str.replace('int', '')
+    assert idx_str.isnumeric()
+    return int(idx_str)
+
+
+def datetime_index_to_str(dt: datetime) -> str:
+    assert isinstance(dt, datetime)
+    return f'ts{dt.timestamp()}'
+
+
+def str_to_datetime_index(idx_str: str) -> datetime:
+    assert isinstance(idx_str, str)
+    assert idx_str.startswith('ts')
+    float_str = idx_str.replace('ts', '')
+    assert float_str.isnumeric()
+    assert float(float_str) >= 0
+    return datetime.fromtimestamp(float(float_str))
